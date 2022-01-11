9ad2960a793e6b66d2720d85be1cbf8c29f0bf79
sutsutman <65862696+sutsutman@users.noreply.github.com>
2022-01-11 19:45:10 +0900

    Add files via upload

---
+++ 부스터/Micro_Mod.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace Micro
+{
+    internal class Micro_Mod : Mod
+    {
+        public Micro_Mod(ModContentPack content) : base(content)
+        {
+            Micro_Mod.Settings = base.GetSettings<Micro_Setting>();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Micro_Setting";
+        }
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Micro_Mod.Settings.DoSettingsWindowContents(inRect);
+        }
+
+        // Token: 0x04000001 RID: 1
+        public static Micro_Setting Settings;
+    }
+}
---
+++ 부스터/Micro_Setting.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace Micro
+{
+    [StaticConstructorOnStartup]
+    public class Micro_Setting : ModSettings
+    {
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref BoosterRot, "ChangeBoosterRot", false);
+        }
+        public void DoSettingsWindowContents(Rect canvas)
+        {
+            Listing_Standard listing_Standard = new Listing_Standard
+            {
+                ColumnWidth = (canvas.width - 34f) / 2f
+            };
+            listing_Standard.Begin(canvas);
+
+            listing_Standard.CheckboxLabeled("BoosterRot".Translate("Change BoosterRot"), ref BoosterRot, null);
+            listing_Standard.Gap(10f);
+            listing_Standard.GapLine(10f);
+
+            //끝
+            listing_Standard.End();
+        }
+        static Micro_Setting()
+        {
+            LongEventHandler.ExecuteWhenFinished(delegate ()
+            { });
+        }
+
+        public static bool BoosterRot = false;
+    }
+}
